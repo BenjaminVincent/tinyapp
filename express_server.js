e0dab011677c1d5aac8e7b327c4f9932a031ca69
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-01-21 01:44:35 +0000

    added generateRandomString() function in express_server.js

--- express_server.js
+++ express_server.js
@@ -4,9 +4,16 @@ const app = express();
 const PORT = 8080;
 
 
-app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+const generateRandomString = () => {
+  return (Math.random().toString(36)+'00000000000000000').slice(2, 8);
+};
+
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
@@ -23,8 +30,8 @@ app.get('/urls', (request, response) => {
   response.render('urls_index', templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.get("/urls/new", (request, response) => {
+  response.render("urls_new");
 });
 
 
@@ -36,6 +43,13 @@ app.get('/urls/:shortURL', (request, response) => {
   
   //console.log("longURL:", request.params);
   response.render('urls_show', templateVars);
+});
+
+
+// post to app
+app.post("/urls", (request, response) => {
+  console.log("OUTPUT: ", request.body);  // Log the POST request body to the console
+  response.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 

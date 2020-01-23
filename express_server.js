f804f97c1ebd00425394e835375c67b3b47eeb72
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-01-23 20:49:33 +0000

    cleaned up code in express_server.js

--- express_server.js
+++ express_server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 const { getUserByEmail, urlsForUser, getRequestUser } = require('./helpers');
@@ -10,10 +9,13 @@ const { getUserByEmail, urlsForUser, getRequestUser } = require('./helpers');
 const app = express();
 const PORT = 8080;
 
+
+
 app.set('view engine', 'ejs');
 
+
+
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
 app.use(cookieSession({
   name: 'session',
   secret: 'string',
@@ -23,119 +25,143 @@ app.use(cookieSession({
 
 
 const generateRandomString = () => {
-  return (Math.random().toString(36)+'00000000000000000').slice(2, 8);
+  return (Math.random().toString(36) + '00000000000000000').slice(2, 8);
 };
 
 
 
 const urlDatabase = {
-  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
-  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" },
   aaaaaa: { longURL: "http://www.youtube.com", userID: "3"}
 };
 
-const users = { 
-  "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
-    password: "purple-monkey-dinosaur"
-  },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
-    password: "dishwasher-funk"
-  },
+
+
+const users = {
   "3": {
     id: "3",
     email: "a@a.com",
     password: "123"
   }
-}
+};
+
+
+/**********************************/
+/*
+                GETS
+*/
+/*********************************/
 
 
 app.get('/urls', (request, response) => {
+
   const id = getRequestUser(request);
   const user = users[id];
   const userURLs = urlsForUser(id, urlDatabase);
 
-  let templateVars = {  
+  let templateVars = {
     user,
-    urls: userURLs 
+    urls: userURLs
   };
+
   if (user) {
     response.render('urls_index', templateVars);
   } else {
     response.render('login', templateVars);
   }
+
 });
 
+
+
 app.get("/urls/new", (request, response) => {
+
   const user = getRequestUser(request);
   const userObj = users[user];
 
-  let templateVars = { 
+  let templateVars = {
     user: userObj,
-    urls: urlDatabase 
+    urls: urlDatabase
   };
 
   if (user) {
-  response.render("urls_new", templateVars);
+    response.render("urls_new", templateVars);
   } else {
     response.render("login", templateVars);
   }
+
 });
 
 
+
 app.get('/urls/:shortURL', (request, response) => {
+
   const shortURL = request.params.shortURL;
   const longURL = urlDatabase[shortURL].longURL;
   const id = getRequestUser(request);
   const userObj = users[id];
   
-  let templateVars = { 
+  let templateVars = {
     user: userObj,
-    shortURL: shortURL, 
-    longURL: longURL 
+    shortURL: shortURL,
+    longURL: longURL
   };
+
   response.render('urls_show', templateVars);
+
 });
 
+
+
 app.get("/u/:shortURL", (request, response) => {
+
   const shortURL = request.params.shortURL;
   const longURL = urlDatabase[shortURL].longURL;
+
   response.redirect(longURL);
+
 });
 
 
 
 app.get('/urls.json', (request, response) => {
+
   response.json(urlDatabase);
+
 });
 
-app.get('/hello', (request, response) => {
-  let templateVars = { greeting: 'Hello World!' };
-  response.render('hello_world', templateVars);
-});
+
 
 app.get('/register', (request, response) => {
-  let templateVars = { 
+
+  let templateVars = {
     user: getRequestUser(request)
   };
+
   response.render('register', templateVars);
+
 });
 
+
+
 app.get('/login', (request, response) => {
+
   const user = getRequestUser(request);
 
   let templateVars = {
     user: user
-  }
+  };
 
   response.render('login', templateVars);
+
 });
 
 
 
+/**********************************/
+/*
+               POSTS
+*/
+/*********************************/
 
 
 
@@ -150,10 +176,15 @@ app.post('/urls/:shortURL/delete', (request, response) => {
   } else {
     response.send("not allowed");
   }
+
   response.redirect(`/urls`);
+
 });
 
+
+
 app.post('/urls/:id', (request, response) => {
+
   const shortURL = request.params.id;
   const longURL = request.body.longURL;
   const userID = request.session.user;
@@ -165,24 +196,31 @@ app.post('/urls/:id', (request, response) => {
   } else {
     response.send("not allowed");
   }
+
 });
 
 
 
 app.post("/urls", (request, response) => {
+
   const longURL = request.body.longURL;
   const rndStr = generateRandomString();
+
   urlDatabase[rndStr] = { longURL, userID: request.session.user };
+
   response.redirect(`/urls/${rndStr}`);
 
 });
 
+
+
 app.post("/login", (request, response) => {
+
   const {email, password} = request.body;
   const user = getUserByEmail(email, users);
 
   if (user) {
-    if (bcrypt.compareSync(password, user.hashedPassword)){
+    if (bcrypt.compareSync(password, user.hashedPassword)) {
       request.session.user = user.id;
     } else {
       response.send("403: Email and or password do not match!");
@@ -190,28 +228,38 @@ app.post("/login", (request, response) => {
   } else {
     response.send("403: account doesn't exist!");
   }
-  response.redirect('/urls')
+
+  response.redirect('/urls');
   
 });
 
+
+
 app.post("/logout", (request, response) => {
-  request.session = null; 
+
+  request.session = null;
   response.redirect('/urls');
+
 });
 
+
+
 app.post('/register', (request, response) => {
+
   const { email, password } = request.body;
   const id = generateRandomString();
   const hashedPassword = bcrypt.hashSync(password, 10);
 
   if (getUserByEmail(email, users)) {
-    response.send("400: email already exists");;
+    response.send("400: email already exists");
   } else {
     users[id] = { id, email, hashedPassword };
     request.session.user = id;
     response.redirect('/urls');
   }
+
 });
+
 
 
 app.listen(PORT, () => {
